f28307204985eb6e3c5bd0aeeb3b13e0dcd98d8b
simat05 <tsimeneh77@gmail.com>
2024-04-17 09:12:20 +0300

    up.js added

---
+++ UP.js
@@ -0,0 +1,8 @@
+console.log("hello world!");
+let firstName;
+firstName="Simeneh";
+let age=22;
+let adress="addis ababa";
+let isStudent=true;
+console.log(firstName + " " + age);
+console.log(`My name is: ${firstName} my address is : ${adress} are you a student: ${isStudent}`);
